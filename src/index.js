81a643f73c4992b35a6b1c5f81ab3eec620b851a
leo-tavares <leonardotavaresdev@gmail.com>
2021-04-06 00:10:56 -0300

    bug fix: use express json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const app = require("express")();
+const express = require("express");
+
+const app = express();
+app.use(express.json());
 
 const todoController = require("./controllers/todo.controller");
 
 app.get("/todo", todoController.getTodo);
 
 app.post("/todo", todoController.setTodo);
-
 
 app.listen(3333, () => console.log("Server up!"));
